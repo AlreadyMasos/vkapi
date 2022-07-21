7fc69dca5335d6e9c77122e6b35401257ad955d5
AlreadyMasos <marura3011@gmail.com>
2022-07-21 14:24:24 +0300

    rework

--- page_objects/RegisterPage.py
+++ page_objects/RegisterPage.py
@@ -7,37 +7,27 @@ from framework.elements.form import Form
 from framework.utils.config_parser import ConfigParser
 from framework.utils.random_util import get_random_password_and_email
 from framework.utils.string_util import validate_timer_string, validate_card
+from page_objects.FileForm import FileForm
 
 
-class CardsPage(BasePage):
+class RegisterPage(BasePage):
+    
     CONFIG = ConfigParser().get_config()
-    _password_path = ('xpath', "//input[@placeholder='Choose Password']", 'pass')
-    _email_path = ('xpath', "//input[@placeholder='Your email']", 'email')
-    _domain_path = ('xpath', "//input[@placeholder='Domain']", 'dom')
-    _page_ind = ('xpath', "//div[@class='page-indicator']", 'ind')
     _timer = ('xpath', '//div[@class="timer timer--white timer--center"]', 'cards')
-    _dropdown_path = ('xpath', '//div[@class="dropdown dropdown--gray"]', 'dropdown')
-    _checkbox_path = ('xpath', '//span[@class="checkbox__box"]', 'checkbox')
-    _next_path = ('xpath', '//a[@class="button--secondary"]', 'next_btn')
-    _org_path = ('xpath', "//div[text()='.org']", 'org')
-    _cookie_accept = ('xpath', '//button[@class="button button--solid button--transparent"]', 'cookie_accept')
-    _cookie_form = ('xpath', '//p[@class="cookies__message"]', 'cookie_form')
-    _help_button = ('xpath', '//button[@class="button button--solid button--blue help-form__send-to-bottom-button"]', 'help')
-    _help_form = ('xpath', '//div[@class="help-form__container"]', 'help_form')
 
-    password_textbox = TextBox(*_password_path)
-    email_textbox = TextBox(*_email_path)
-    domain_textbox = TextBox(*_domain_path)
-    page_ind_text = Text(*_page_ind)
-    button_drop = Button(*_dropdown_path)
-    accept_checkbox = CheckBox(*_checkbox_path)
-    next_button = Button(*_next_path)
-    org_button = Button(*_org_path)
+    password_textbox = TextBox('xpath', "//input[@placeholder='Choose Password']", 'pass')
+    email_textbox = TextBox('xpath', "//input[@placeholder='Your email']", 'email')
+    domain_textbox = TextBox('xpath', "//input[@placeholder='Domain']", 'dom')
+    page_ind_text = Text('xpath', "//div[@class='page-indicator']", 'ind')
+    button_drop = Button('xpath', '//div[@class="dropdown dropdown--gray"]', 'dropdown')
+    accept_checkbox = CheckBox('xpath', '//span[@class="checkbox__box"]', 'checkbox')
+    next_button = Button('xpath', '//a[@class="button--secondary"]', 'next_btn')
+    org_button = Button('xpath', "//div[text()='.org']", 'org')
     timer = TextBox(*_timer)
-    accept_cookie_button = Button(*_cookie_accept)
-    cookie_form = TextBox(*_cookie_form)
-    help_button = Button(*_help_button)
-    help_form = Form(*_help_form)
+    accept_cookie_button = Button('xpath', '//button[@class="button button--solid button--transparent"]', 'cookie_accept')
+    cookie_form = TextBox('xpath', '//p[@class="cookies__message"]', 'cookie_form')
+    help_button = Button('xpath', '//button[@class="button button--solid button--blue help-form__send-to-bottom-button"]', 'help')
+    help_form = Form('xpath', '//div[@class="help-form__container"]', 'help_form')
 
     def __init__(self):
         super().__init__(*self._timer)
