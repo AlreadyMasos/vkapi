834ac16d8f37f2791ecbbfa6423fdf191695df12
AlreadyMasos <marura3011@gmail.com>
2022-07-27 08:49:13 +0300

    last last rework

--- tests/tester.py
+++ tests/tester.py
@@ -5,21 +5,23 @@ from framework.utils.cfg_parser import ConfigParser
 def test_step():
     cfg = ConfigParser().get_config()
     resp_first = Request(*cfg['first_req'])
-    assert resp_first.check_code_200()
-    assert resp_first.check_if_json()
-    assert resp_first.check_if_sorted_by_id()
+    assert resp_first.check_code_200(), 'wrong code'
+    assert resp_first.check_if_json(), 'not json'
+    assert resp_first.check_if_sorted_by_id(), 'not sorted by id'
     resp_second = Request(*cfg['second_req'])
-    assert resp_second.check_code_200()
-    assert resp_second.check_99_post()
+    assert resp_second.check_code_200(), 'wrong code'
+    assert resp_second.check_99_post(), 'wrong post information'
     resp_third = Request(*cfg['third_req'])
-    assert resp_third.check_empty()
-    assert resp_third.check_code_404()
+    assert resp_third.check_empty(), 'not empty json'
+    assert resp_third.check_code_404(), 'wrong code'
     resp_fourth = Request(*cfg['fourth_req'])
-    assert resp_fourth.check_correct_post_req() and resp_fourth.check_code_201()
+    assert resp_fourth.check_correct_post_req(), 'wrong post'
+    assert resp_fourth.check_code_201(), 'wrong code'
     resp_fifth = Request(*cfg['fifth_req'])
-    assert resp_fifth.check_if_json() and resp_fifth.check_code_200()
+    assert resp_fifth.check_if_json(), 'not json'
+    assert resp_fifth.check_code_200(), 'wrong code'
     user_with_id = resp_fifth.find_user_by_id(cfg['user_id'])
-    assert resp_fifth.check_correct_user(user_with_id)
+    assert resp_fifth.check_correct_user(user_with_id), 'wrong user information'
     resp_sixth = Request(*cfg['sixth_req'])
-    assert resp_sixth.check_code_200()
-    assert resp_sixth.check_correct_user(resp_sixth.get_json())
+    assert resp_sixth.check_code_200(), 'wrong code'
+    assert resp_sixth.check_correct_user(resp_sixth.get_json()), 'wrong user information'
